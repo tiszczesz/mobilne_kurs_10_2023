019d6cf8971b10231e7e47a2d178d0b709465fcc
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-10-13 16:24:09 +0200

    ddd

--- android_projects/cw1/app/src/main/java/com/example/cw1/MainActivity.kt
+++ android_projects/cw1/app/src/main/java/com/example/cw1/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.cw1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var _counter:Int = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button3)
+        val info = findViewById<TextView>(R.id.textView2)
+        val counterView = findViewById<TextView>(R.id.tvCounter)
+        button.setOnClickListener {
+            _counter = (_counter+1)%10;
+          counterView.text = _counter.toString()
+        }
     }
 }
