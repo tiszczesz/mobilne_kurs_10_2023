d876d0b47f57be7930c7bda7df0fef044634db27
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-10-21 13:16:16 +0200

    hhh

---
+++ android_projects/cw4/app/src/main/java/com/example/cw4/FirstFragment.kt
@@ -0,0 +1,9 @@
+package com.example.cw4
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+}
---
+++ android_projects/cw4/app/src/main/java/com/example/cw4/SecondFragment.kt
@@ -0,0 +1,8 @@
+package com.example.cw4
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+}
