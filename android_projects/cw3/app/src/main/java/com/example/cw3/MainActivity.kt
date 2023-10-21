98f14ed55c20f747ee7c3a7d1a080b975ecb0ba7
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2023-10-21 09:46:48 +0200

    ggg

--- android_projects/cw3/app/src/main/java/com/example/cw3/MainActivity.kt
+++ android_projects/cw3/app/src/main/java/com/example/cw3/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.example.cw3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val btn2 = findViewById<Button>(R.id.btn2)
         Log.d("CYCLE","OnCreate")
+
+        btn1.setOnClickListener {
+            //Toast.makeText(this@MainActivity,"do drugiej",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+        btn2.setOnClickListener {
+           // Toast.makeText(this@MainActivity,"do trzeciej",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this,ThirdActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onStart() {
